c33fe9f8106ed27bfac4191c78e5f8531f71df30
Галков Вадим Игоревич <GalkovVI@nornik.ru>
2022-09-21 05:25:48 +0700

    make it better

--- main.go
+++ main.go
@@ -169,5 +169,5 @@ func main() {
 	gr.POST("/login", Login)
 	gr.POST("/register", Register)
 
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(os.Getenv("PORT"))
 }
